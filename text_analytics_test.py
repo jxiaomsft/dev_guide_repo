6e51e0c33bfcdb9356d3fdb7495bc9fd09bade4b
jasonxiaoda <jasonxiaoda@berkeley.edu>
2020-08-19 14:58:32 -0700

    adding a language detection feature

--- text_analytics_test.py
+++ text_analytics_test.py
@@ -28,6 +28,18 @@ def authenticate_client():
 
 client = authenticate_client()
 
+# Detecting Languages:
+language_api_url = endpoint + "/text/analytics/v3.0/languages"
+documents = {"documents": [
+    {"id": "1", "text": "This is a document written in English."},
+    {"id": "2", "text": "Este es un document escrito en Español."},
+    {"id": "3", "text": "这是一个用中文写的文件"}
+]}
+headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+response = requests.post(language_api_url, headers=headers, json=documents)
+languages = response.json()
+pprint(languages)
+
 
 # Analyzing Sentiment:
 sentiment_url = endpoint + "/text/analytics/v3.0/sentiment"
