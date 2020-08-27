d537256a66a5a960918304472d3d48a403c39eee
jasonxiaoda <jasonxiaoda@berkeley.edu>
2020-08-27 13:11:18 -0700

    skeleton added

---
+++ ta_skeleton.py
@@ -0,0 +1,72 @@
+import os
+import requests
+from pprint import pprint
+# These packages may need to be installed using pip
+# pip install azure-ai-textanalytics
+# pip install azure-core-credentials
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+# from dotenv import load_dotenv
+# load_dotenv()
+subscription_key = ...
+endpoint = ...
+
+# Authentication
+def authenticate_client():
+    ta_credential = AzureKeyCredential(subscription_key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint= endpoint, credential=ta_credential)
+    return text_analytics_client
+
+# Perform sentiment analysis
+def sentiment_analysis_example(client, documents):
+    sentiment_url = endpoint + "/text/analytics/v3.0/sentiment"
+    headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+    response = requests.post(sentiment_url, headers=headers, json=documents)
+    sentiments = response.json()
+
+    print("Printing sentiments ... \n")
+    pprint(sentiments)
+    return sentiments
+
+# Extract key phrases
+def extract_key_phrases(client, documents):
+    keyphrase_url = endpoint + "/text/analytics/v3.0/keyphrases"
+    headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+    response = requests.post(keyphrase_url, headers=headers, json=documents)
+    key_phrases = response.json()
+
+    print("Printing key phrases ... \n")
+    pprint(key_phrases)
+    return key_phrases
+
+def identify_entities(client, documents):
+    entities_url = endpoint + "/text/analytics/v3.0/entities/recognition/general"
+    headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+    response = requests.post(entities_url, headers=headers, json=documents)
+    entities = response.json()
+    pprint(entities)
+
+          
+
+if __name__ == "__main__":
+    data = pd.read_csv("data.txt")
+    # data.columns.values
+    # summary = data["Theme"].tolist() <-- this is the list of "themes" which will be replaced by the text ticket
+    # print(type(summary))
+    documents = {"documents": [
+        {"id": "1", "language": "en",
+            "text": "I do not like this hammer made by Black & Decker. It does not work correctly. I want to request a return."},
+        {"id": "2", "language": "es",
+            "text": "I've been trying to talk to someone about my sink problem. It won't hold all of my fish."}
+    ]}
+
+    # Replace documents with data of your own if you want to analyze.
+    your_own_documents = ...
+
+    client = authenticate_client()
+    sentiments = sentiment_analysis_example(client, documents)
+    key_phrases = extract_key_phrases(client, documents)
+    identify_entities(client, documents)
+
